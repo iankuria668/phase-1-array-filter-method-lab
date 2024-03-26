c11558321634d8302e12344c1e41467a945d07fe
iankuria668 <iankuria668@gmail.com>
2024-03-26 14:54:32 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(names, confirmTable) {
+    return names.filter(names => names.toLowerCase() === confirmTable.toLowerCase());
+}
+
+function fuzzyMatch(names,confirmTable){
+    return names.filter(drivers => drivers.toLowerCase().startsWith(confirmTable.toLowerCase()));
+}
+
+const element = drivers.filter(matchName);
+
+function matchName(drivers, homeTown){
+    return drivers.filter(drivers => drivers=== homeTown.toLowerCase());
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
